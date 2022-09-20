18f65896da353964c83e58e02fa93899c0a3c1ba
cge1023 <ge931023@gmail.com>
2022-09-20 18:52:34 +0900

    회원가입 로그인 뷰 수정 / 네비바 아이콘 추가

--- app.js
+++ app.js
@@ -6,25 +6,27 @@ const dotenv = require("dotenv");
 const { sequelize } = require("./models");
 const port = 8000;
 const db = require("./models");
-dotenv.config();//환경 변수용
+dotenv.config(); //환경 변수용
 
 const app = express();
-app.use(session({
-    resave:false,
+app.use(
+  session({
+    resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-        httpOnly: true,
-        secure: false,
-        maxAge:1000000,
-    }, 
-})); // 세션객체 설정
+      httpOnly: true,
+      secure: false,
+      maxAge: 1000000,
+    },
+  })
+); // 세션객체 설정
 
-const passport = require('passport'); // js에서 index.js파일은 파일명을 생략할 수 가 있다
-const passportConfig = require('./passport');
+const passport = require("passport"); // js에서 index.js파일은 파일명을 생략할 수 가 있다
+const passportConfig = require("./passport");
 passportConfig(); //passport 설정
-app.use(passport.initialize());//req에 passport 설정을 심는다
-app.use(passport.session());// req.session에 passport 정보를 저장한다 {express-session과 연동하는 것}
+app.use(passport.initialize()); //req에 passport 설정을 심는다
+app.use(passport.session()); // req.session에 passport 정보를 저장한다 {express-session과 연동하는 것}
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
 
@@ -32,20 +34,21 @@ app.set("view engine", "ejs");
 app.use(morgan("dev")); // 패킷 정보 공개
 app.use("/static", express.static("static"));
 app.use("/uploads", express.static("uploads"));
-app.use(express.json());//json파싱
-app.use(express.urlencoded({extended:false}));//인코딩된 url파싱
-app.use(cookieParser(process.env.COOKIE_SECRET));//쿠키에 암호 넣고 파싱함
+app.use(express.json()); //json파싱
+app.use(express.urlencoded({ extended: false })); //인코딩된 url파싱
+app.use(cookieParser(process.env.COOKIE_SECRET)); //쿠키에 암호 넣고 파싱함
 
-sequelize.sync({force:false})
-    .then(()=>{
-        console.log("데이터베이스 연결 성공");
-    })
-    .catch((err)=>{
-        console.error(err);
-    }); // DB연결
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  }); // DB연결
 
-app.use("/",indexRouter); // index router 로 이동
-app.use("/auth",authRouter); // auth router 사용
+app.use("/", indexRouter); // index router 로 이동
+app.use("/auth", authRouter); // auth router 사용
 
 app.use((err, req, res, next) => {
   res.render("error", { error: err.message });
--- routes/auth.js
+++ routes/auth.js
@@ -1,74 +1,71 @@
-const express = require('express');
-const passport = require('passport');
-const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
-const {User} = require("../models");
+const express = require("express");
+const passport = require("passport");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const { User } = require("../models");
 
 const router = express.Router();
 
-router.post('/signup',isNotLoggedIn,async(req,res,next)=>{
-    const {email, nickName, password, birthday} = req.body;
-    console.log(req.body);
-    try{
-        const exUser = await User.findOne({where:{email}});
-        if (exUser){
-            return res.send({code:400});// 실패
-        }
-        await User.create({
-            email,
-            nickName,
-            password,
-            birthday
-        });
-        return res.send({code:200}); //성공
-    }catch(error){
-        console.error(error);
-        return next(error);
+router.post("/signup", isNotLoggedIn, async (req, res, next) => {
+  const { email, nickName, password, birthday } = req.body;
+  console.log(req.body);
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.send({ code: 400 }); // 실패
     }
+    await User.create({
+      email,
+      nickName,
+      password,
+      birthday,
+    });
+    return res.send({ code: 200 }); //성공
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
 });
 
-
-router.get("/login",(req,res)=>{
-    res.render("login");
+router.get("/login", (req, res) => {
+  res.render("login");
 });
-router.get("/join",(req,res)=>{
-    res.render("signup");
-})
-
-router.post('/login',isNotLoggedIn,(req,res,next)=>{
-    passport.authenticate('local',(authError,user,info)=>{
-        if(authError){
-            console.error(authError);
-            return res.send({code:400});
-        }
-        if (!user){
-           
-            return res.send({code:400});
-        }
-        return req.login(user,(loginError)=>{
-            if(loginError){
-                console.error(loginError);
-                return res.send({code:400});
-            }
-            return res.send({code:200});
-        }); 
-    })(req,res,next);
+router.get("/join", (req, res) => {
+  res.render("signup");
 });
 
-
-
-
-
-
-router.get('/logout',isLoggedIn,(req,res)=>{
-    req.logout();
-    req.session.destroy();
-    res.redirect('/');
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  passport.authenticate("local", (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return res.send({ code: 400 });
+    }
+    if (!user) {
+      return res.send({ code: 400 });
+    }
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return res.send({ code: 400 });
+      }
+      return res.send({ code: 200 });
+    });
+  })(req, res, next);
 });
 
-router.get('/kakao',passport.authenticate('kakao'));
-router.get('/kakao/callback',passport.authenticate('kakao',{
-    failureRedirect:'/'
-}),(req,res)=>{
-    res.redirect('/');
+router.get("/logout", isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect("/");
 });
+
+router.get("/kakao", passport.authenticate("kakao"));
+router.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
 module.exports = router;

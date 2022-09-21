129344cf9026d25376160d3a2a0ad383bcaf186f
Ungzzi <qhshqhsh0225@gmail.com>
2022-09-21 16:59:19 +0900

    프로필 네비바 추가

--- models/post.js
+++ models/post.js
@@ -1,34 +1,34 @@
 const Sequelize = require("sequelize");
-module.exports= class Post extends Sequelize.Model{
-    static init(sequelize){
+module.exports = class Post extends Sequelize.Model {
+    static init(sequelize) {
         return super.init({
-            content:{
+            content: {
                 type: Sequelize.STRING(100),
                 allowNull: false,
             },
-            location:{
+            location: {
                 type: Sequelize.STRING(100),
-                allowNull:true
+                allowNull: true
             }
-        },{
+        }, {
             sequelize,
             timestamps: true,
-            underscored:false,
-            modelName:'Post',
-            tableName:"posts",
-            paranoid:false,
-            charset:"utf8",
-            collate:"utf8_general_ci"
+            underscored: false,
+            modelName: 'Post',
+            tableName: "posts",
+            paranoid: false,
+            charset: "utf8",
+            collate: "utf8_general_ci"
         });
     }
-    static associate(db){
-        db.Post.belongsTo(db.User,{foreignKey:"UserId",sourceKey:"id",onDelete: "cascade",onUpdate:"cascade"});
-        db.Post.belongsToMany(db.User,{through:"bookmark",onDelete: "cascade",onUpdate:"cascade"});
-        db.Post.hasMany(db.PostMedia,{foreignKey:"PostId",targetKey:"id",onDelete: "cascade",onUpdate:"cascade"});
-        db.Post.hasMany(db.HashTag,{
-            'foreignKey':"PostId",
-            "TargetKey":"id"
-            ,onDelete: "cascade",onUpdate:"cascade"
+    static associate(db) {
+        db.Post.belongsTo(db.User, { foreignKey: "UserId", sourceKey: "id", onDelete: "cascade", onUpdate: "cascade" });
+        db.Post.belongsToMany(db.User, { through: "bookmark", onDelete: "cascade", onUpdate: "cascade" });
+        db.Post.hasMany(db.PostMedia, { foreignKey: "PostId", targetKey: "id", onDelete: "cascade", onUpdate: "cascade" });
+        db.Post.hasMany(db.HashTag, {
+            'foreignKey': "PostId",
+            "TargetKey": "id"
+            , onDelete: "cascade", onUpdate: "cascade"
         });
     }
 };

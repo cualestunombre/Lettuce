37ca6177b01708ce2ae1a0df920a22df182d5524
Malbongs <qothgus13@naver.com>
2022-09-22 17:08:51 +0900

    게시글

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,35 @@
 const { application } = require("express");
 const express = require("express");
 const router = express.Router();
-const {User,Post,PostMedia} = require("../models");
+const {User,Post,PostMedia,Follow} = require("../models");
 
 
 
 
 //게시글 가져 오기
+// 모든 유저 게시물 const data = await Post.findAll({raw:true,include:[{model:PostMedia},{model:User,attributes:['nickName','email','profile']}]});
 router.get("/",async(req,res) =>{
     if(req.isAuthenticated()){
-        const data = await Post.findAll({raw:true,include:[{model:PostMedia},{model:User,attributes:['nickName','email','profile']}]});
-        console.log(data);
-        res.render("main",{data:data});  
+        const arr=[];
+        const FollowingList = await User.findAll({
+            raw: true,
+            attributes: ['id'],
+            include: [{ model: User, as: 'followings', where: { id: req.user.id } }]
+        })
+        for (let i=0; i<FollowingList.length ;i++){
+            const FollowingPost = await Post.findAll({
+                raw:true,
+                include: [{model:PostMedia},{model:User,attributes:['nickName','profile']}]
+            })
+            FollowingPost.forEach(ele=>{
+                arr.push(ele);
+            });
+        }arr.sort((a,b)=>{
+            return a['Postmedia.createdAt'] -b['Postmedia.createdAt'];
+        })
+
+         console.log(arr);
+          
     }
     else{
         res.redirect("/auth/login");

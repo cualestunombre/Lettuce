fd2103ca173179bd8702668b3043afb43df2f7b3
Malbongs <qothgus13@naver.com>
2022-09-25 18:47:36 +0900

    댓글

--- routes/comment.js
+++ routes/comment.js
@@ -3,34 +3,41 @@ const router = express.Router();
 const {Comment,User} = require("../models");
 
 //댓글 등록
-router.post("/comments",async(req,res)=>{
+router.post("/comments",async(req,res)=>{ console.log(req.body);
     const comments = await Comment.create({
-        PostId:req.body.pid,
+        PostId:req.body.postId,
         UserId:req.user.id,
         comment:req.body.comment
     });
-    console.log(comments);
-    const come  = await User.findOne({
-        attributes:["nickName","profile"],
-        where:{id:req.user.id}
-    });
-    const data = {
-        comment:comments.comment,
-        nickName: come.nickName,
-        profile: come.profile
-    }
 
-    res.send(data);
+    res.send({code:200});
 })
 
-//댓글 삭제
+//댓글 불러오기
+
+router.get("/comments",async(req,res)=>{
+    const come  = await Comment.findAll({
+        raw:true,
+        attributes:["comment","createdAt","id"],
+        include:[{model:User, attributes:["nickName","profile","id"]}],
+        where:{UserId:req.user.id,
+                PostId:req.query.PostId},
+        order: [['createdAt','DESC']],
+        limit:2
+
+    });
+    res.send(come);
+})
 
 
 
 
 //댓글 삭제
 router.post("/commentDelete",async(req,res)=>{
-
+    await Comment.destroy({
+        where:{id:req.body.id}
+    });
+    res.send({code:200});
 })
 
 module.exports = router;

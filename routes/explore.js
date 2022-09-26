c9852c0ee836b25fd37a7a2f1b3b483c63ab83fb
Ungzzi <qhshqhsh0225@gmail.com>
2022-09-26 18:47:01 +0900

    project: 전체게시글&태그게시글 모달창 구현 완료

--- routes/explore.js
+++ routes/explore.js
@@ -1,13 +1,76 @@
 
 const express = require("express");
 const router = express.Router();
-const { User, Post, PostMedia, Follow, HashTag } = require("../models");
+const { User, Post, PostMedia, Follow, HashTag, Like } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 
 router.get("/", async (req, res, next) => {
     res.render("explore");
 });
 
+router.get("/board", isLoggedIn, async (req, res, next) => {
+    const list = [];
+    const arr = [];
+    const post = await Post.findAll({
+        raw: true,
+        attributes: ['content', 'createdAt', 'id'],
+        where: { id: req.query.id },
+        include: [{ model: PostMedia, attributes: ['createdAt', 'type', 'src'] }, { model: User, attributes: ['id', 'nickName', 'profile'] }]
+    })
+
+    post.forEach(ele => {
+        arr.push(ele);
+    });
+
+    for (let i = 0; i < arr.length; i++) {
+        let flag = true;
+        for (let j = 0; j < list.length; j++) {
+            if (list[j].id == arr[i].id) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag == false) {
+            list[list.length - 1].src.push({ src: arr[i]['Postmedia.src'], type: arr[i]['Postmedia.type'] });
+        }
+        else {
+            list.push(arr[i]);
+            list[list.length - 1].src = [{ src: arr[i]['Postmedia.src'], type: arr[i]['Postmedia.type'] }];
+        }
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        let date = list[i].createdAt;
+        let sendDate = date.getFullYear() + '년 ' + (parseInt(date.getMonth()) + 1) + '월 ' + date.getDate() + "일 ";
+        if (date.getHours() < 12) {
+            sendDate += "오전 " + date.getHours() + "시 ";
+        }
+        else {
+            sendDate += '오후 ' + (parseInt(date.getHours()) - 12) + "시 ";
+        }
+        sendDate += +date.getMinutes() + "분";
+
+        list[i].createdAt = sendDate;
+        delete list[i]['Postmedia.createdAt'];
+        delete list[i]['Postmedia.type'];
+        delete list[i]['Postmedia.src'];
+    }
+
+
+    const data = await Like.findAll({ where: { UserId: req.user.id, PostId: req.query.id } });
+    const cnt = await Like.findAll({ where: { PostId: req.query.id } });
+    list[0].likeCount = cnt.length;
+    if (data.length != 0) {
+        post.like = 'true';
+    }
+
+    if (post.length == 0) {
+        res.send({ code: 400 });
+    }
+    else {
+        res.send({ data: list[0], code: 200 });
+    }
+});
 
 //게시글 가져 오기
 // 모든 유저 게시물 const data = await Post.findAll({raw:true,include:[{model:PostMedia},{model:User,attributes:['nickName','email','profile']}]});

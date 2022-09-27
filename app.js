f9e9a1912f7bc546d5d609ff0c96a8439c6fc63f
Malbongs <qothgus13@naver.com>
2022-09-27 14:18:54 +0900

    카카오 로그인 완료

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
   res.render("nonmatch");
 });
 app.use((err, req, res, next) => {
-  res.render("error", { error: err.message });
+  res.render("error", { error: err });
 });
 const server = app.listen(port, () => {
   console.log("Server Port : ", port);
